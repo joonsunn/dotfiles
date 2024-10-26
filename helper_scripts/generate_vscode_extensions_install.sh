0a469823f1643295dbdaebb3817029aa820c1f68
Joon Sunn Foo <joonsunn.foo@gmail.com>
2024-10-26 21:37:32 +0800

    add stow ignore; add helper scripts to backup and restore vscode extensions

---
+++ helper_scripts/generate_vscode_extensions_install.sh
@@ -0,0 +1,3 @@
+rm -rf ./install_vscode_extensions.sh
+echo "#!/bin/bash" > ./install_vscode_extensions.sh
+code --list-extensions | xargs -L 1 echo code --install-extension >> ./install_vscode_extensions.sh
---
+++ helper_scripts/install_vscode_extensions.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+code --install-extension esbenp.prettier-vscode
