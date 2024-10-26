d33502aa115c25f1d9af31bdc66f15e6141ac477
Joon Sunn Foo <joonsunn.foo@gmail.com>
2024-10-26 21:40:31 +0800

    update extensions list

--- helper_scripts/install_vscode_extensions.sh
+++ helper_scripts/install_vscode_extensions.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
+code --install-extension davidanson.vscode-markdownlint
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
 code --install-extension esbenp.prettier-vscode
