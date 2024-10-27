435914c859274058ab4ea02f37206d1d9ec71df3
Joon Sunn, Foo <97303880+joonsunn@users.noreply.github.com>
2024-10-27 16:13:31 +0800

    Update run_first.sh
    updated github links to public urls

--- helper_scripts/run_first.sh
+++ helper_scripts/run_first.sh
@@ -6,4 +6,4 @@ chsh -s $(which zsh)
 
 curl -s https://ohmyposh.dev/install.sh | bash -s
 oh-my-posh font install
-git clone git@github.com:zsh-users/zsh-syntax-highlighting.git
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
