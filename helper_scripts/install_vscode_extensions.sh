fc195e64d3baad6affc426904039782ec2ba698f
Joon Sunn Foo <joonsunn.foo@gmail.com>
2025-05-05 22:13:38 +0800

    feat: add wezterm

--- helper_scripts/install_vscode_extensions.sh
+++ helper_scripts/install_vscode_extensions.sh
@@ -1,13 +1,13 @@
-#!/bin/zsh
-
+#!/bin/bash
+code --install-extension alefragnani.bookmarks
+code --install-extension bradlc.vscode-tailwindcss
+code --install-extension codeium.codeium
 code --install-extension davidanson.vscode-markdownlint
 code --install-extension dbaeumer.vscode-eslint
-code --install-extension eamodio.gitlens
-code --install-extension esbenp.prettier-vscode
-code --install-extension alefragnani.bookmarks
-code --install-extension codeium.codeium
 code --install-extension donjayamanne.githistory
 code --install-extension dsznajder.es7-react-js-snippets
+code --install-extension eamodio.gitlens
+code --install-extension esbenp.prettier-vscode
 code --install-extension eserozvataf.one-dark-pro-monokai-darker
 code --install-extension formulahendry.auto-close-tag
 code --install-extension formulahendry.auto-rename-tag
@@ -18,6 +18,7 @@ code --install-extension pkief.material-icon-theme
 code --install-extension prisma.prisma
 code --install-extension sallar.json-to-js-object
 code --install-extension usernamehw.errorlens
+code --install-extension walnuts1018.oklch-vscode
 code --install-extension wix.vscode-import-cost
 code --install-extension yoavbls.pretty-ts-errors
 code --install-extension yzhang.markdown-all-in-one
